a53f1910559cc8746e547f8e18087bffd97b471c
David Vargas <vargasd2106@gmail.com>
2018-07-22 17:11:27 -0600

    creacion de la archivo assets.php

---
+++ Assets/Assets.php
@@ -0,0 +1,11 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="../../favicon.ico" />
+<link href="<?php echo URL;?>Assets/CSS/Bootstrap/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link rel="stylesheet" href="<?php echo URL;?>Assets/CSS/Bootstrap/fonts/css/font-awesome.min.css">
+<link href="<?php echo URL;?>Assets/CSS/footer.css" rel="stylesheet" media="screen">
+<link href="<?php echo URL;?>Assets/CSS/datatables.min.css" rel="stylesheet" type="text/css">
+<script src="<?php echo URL;?>Assets/JS/bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo URL;?>Assets/JS/jquery-1.11.3-jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo URL;?>Assets/JS/jquery-3.1.0.min.js"></script>
+<script type="text/javascript" src="<?php echo URL;?>Assets/JS/datatables.min.js"></script>
